fff824aef5453c30e586d180b3b2fe04c32ee4bb
Diego Cabre <diegocabre@gmail.com>
2023-11-15 23:38:58 -0300

    comenzando proyecto

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,46 +1,54 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect} from "react";
 import { MyContext } from "../context/MyContext";
 import { useNavigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import "./Home.css";
 
 export const Home = () => {
-  const [selectpizzas, setSelectPizzas] = useState(null);
   const { pizzas } = useContext(MyContext);
   const navigate = useNavigate();
+  const [selectpizzas, setSelectPizzas] = useState(null);
 
   useEffect(() => {
     setSelectPizzas(pizzas);
   }, [pizzas]);
+  const getPizzas = async () => {
+    if (!pizzas) return;
+    navigate(`/pizza/${pizzas[0].name}`);
+  };
 
   return (
-    <div>
-      <div className="grid container d-flex justify-content-center mt-5 mb-5 p-5 bg-light rounded shadow w-75 mx-auto">
+    <div className="container d-flex justify-content-center mt-5 mb-5 p-5 bg-light rounded shadow w-75 mx-auto">
         {selectpizzas?.map((pizza) => (
-          <div className="col" key={pizza.id}>
-            <div className="card h-100">
-              <img src={pizza.img} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h3 className="card-title">{pizza.name}</h3>
-                <li className="card-text">{pizza.ingredients[0]}</li>
-                <li className="card-text">{pizza.ingredients[1]}</li>
-                <li className="card-text">{pizza.ingredients[2]}</li>
-                <li className="card-text">{pizza.ingredients[3]}</li>
-              </div>
-              <div className="card-footer">
-                <div>
-                  <small className="text-muted">{pizza.price}$</small>
-                </div>
-                <Button onClick={() => navigate(`/pizza/ ${pizza.name}`)}>
-                  Ver más
-                </Button>
-                <Button onClick={() => navigate(`/carrito/${pizza.price}`)}>
-                  Añadir
-                </Button>
-              </div>
-            </div>
-          </div>
+        <Card className="d-flex flex-column align-items-center"  key={pizza.id} style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={pizza.img} />
+            <Card.Body>
+            <Card.Title>{pizza.name}</Card.Title>
+            <Card.Text>
+                <li>{pizza.ingredients[0]}</li>
+            </Card.Text>
+            <Card.Text>
+            <li>{pizza.ingredients[1]}</li>
+            </Card.Text>
+            <Card.Text>
+            <li>{pizza.ingredients[2]}</li>
+            </Card.Text>
+            <Card.Text>
+            <li>{pizza.ingredients[3]}</li>
+            </Card.Text>
+            <Card.Text>
+                <p>Precio: ${pizza.price}</p>
+            </Card.Text>
+            <Button variant="primary" onClick={() => getPizzas()}>
+                Ver más
+            </Button>
+            <Button variant="primary" onClick={() => navigate(`/carrito/${pizza.name}`)}>
+                Agregar al carrito
+            </Button>
+            </Card.Body>
+        </Card>
         ))}
-      </div>
     </div>
   );
-};
+}
--- src/views/Pizza.jsx
+++ src/views/Pizza.jsx
@@ -1,49 +1,41 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { MyContext } from "../context/MyContext";
 export const Pizza = () => {
-    const [selectpizzas, setSelectPizzas] = useState(null);
-    const { pizzas } = useContext(MyContext);
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const [selectpizzas, setSelectPizzas] = useState(null);
+  const { pizzas } = useContext(MyContext);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        setSelectPizzas(pizzas);
-    }, [pizzas]);
+  useEffect(() => {
+    setSelectPizzas(pizzas);
+  }, [pizzas]);
 
-    return (
-        <div>
-            <div className="grid container d-flex justify-content-center mt-5 mb-5 p-5 bg-light rounded shadow w-75 mx-auto">
-                {selectpizzas?.map((pizza) => (
-                    <div className="col" key={pizza.id}>
-                        <div className="card h-100">
-                            <img
-                                src={pizza.img}
-                                className="card-img-top"
-                                alt="..."
-                            />
-                            <div className="card-body">
-                                <h3 className="card-title">{pizza.name}</h3>
-                                <li className="card-text">{pizza.ingredients[0]}</li>
-                                <li className="card-text">{pizza.ingredients[1]}</li>
-                                <li className="card-text">{pizza.ingredients[2]}</li>
-                                <li className="card-text">{pizza.ingredients[3]}</li>
-                            </div>
-                            <div className="card-footer">
-                                <div>
-                                    <small className="text-muted">{pizza.price}$</small>
-                                </div>
-                                <Button
-                                    onClick={() => navigate(`/carrito/${pizza.id}`)}
-                                    className="btn btn-primary"
-                                >
-                                    Añadir
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div>
+      <div className="grid container d-flex justify-content-center mt-5 mb-5 p-5 bg-light rounded shadow w-75 mx-auto">
+        {selectpizzas?.map((pizza) => (
+          <div className="col" key={pizza.id}>
+            <div className="card h-100">
+              <img src={pizza.img} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h3 className="card-title">{pizza.name}</h3>
+                <li className="card-text">{pizza.ingredients[0]}</li>
+                <li className="card-text">{pizza.ingredients[1]}</li>
+                <li className="card-text">{pizza.ingredients[2]}</li>
+                <li className="card-text">{pizza.ingredients[3]}</li>
+              </div>
+              <div className="card-footer">
+                <div>
+                  <small className="text-muted">{pizza.price}</small>
+                </div>
+                <Button onClick={() => navigate(`/carrito/${pizza.name}`)}>
+                  Agregar al carrito
+                </Button>
+              </div>
             </div>
-        </div>
-    );
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
